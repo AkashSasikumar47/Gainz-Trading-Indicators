fda58d093124bf548228ff17b9ff03483a03ca9f
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-15 15:25:03 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>GAINZ Trading Indicators</h1>
+        <p>Your Source for Technical Analysis</p>
       </header>
+      <main className="main-content">
+        <section className="indicator-card">
+          <h2>Indicator 1: Trading Volume</h2>
+          <p>
+            Trading Volume is a fundamental indicator that provides insights
+            into market activity and liquidity.
+          </p>
+          <pre>
+            <code>Code: // Place your code here</code>
+          </pre>
+        </section>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2023 Akash Sasikumar</p>
+      </footer>
     </div>
   );
 }
