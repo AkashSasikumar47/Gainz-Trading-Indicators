6385a5af8d55d68c69e723bedad4f027d47d432c
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-16 01:41:28 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>GAINZ Trading Indicators</h1>
-        <p>Your Source for Technical Analysis</p>
-      </header>
-      <main className="main-content">
-        <section className="indicator-card">
-          <h2>Indicator 1: Trading Volume</h2>
-          <p>
-            Trading Volume is a fundamental indicator that provides insights
-            into market activity and liquidity.
-          </p>
-          <pre>
-            <code>Code: // Place your code here</code>
-          </pre>
-        </section>
+      <Navbar />
+
+      <main>
+        <Hero />
+        <About />
       </main>
-      <footer className="footer">
-        <p>&copy; 2023 Akash Sasikumar</p>
-      </footer>
+
+      <Footer />
     </div>
   );
 }
