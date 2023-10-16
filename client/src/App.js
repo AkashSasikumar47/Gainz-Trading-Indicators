379c5f269dcb588539e6e6bbdfc0415816ccccf7
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-17 00:34:14 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Explore from './components/Explore/Explore';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Explore />
       </main>
 
       <Footer />
