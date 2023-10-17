5ca3e39a834f2f345a4f89a0c74d9968f3bab64b
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-17 11:53:06 +0530

    Create Aboutgainz.js

---
+++ client/src/components/Home/Aboutgainz/Aboutgainz.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Aboutgainz.css';
+
+function AboutGainz() {
+    return (
+        <div className="AboutGainz">
+            <div className="AboutGainzContent">
+                <div className="AboutGainzTitle">About GAINZ</div>
+                <div className="AboutGainzSubtitle">Empowering Traders with Insights</div>
+                <div className="AboutGainzDescription">
+                    Welcome to GAINZ, your ultimate resource for unlocking the power of technical indicators in the world of stock trading. Created by Akash Sasikumar, a Full-Stack Developer and Trader specializing in equity and derivatives, GAINZ is your gateway to understanding market trends, making informed trading decisions, and enhancing your trading strategies.
+                </div>
+            </div>
+            <div className="AboutGainzImage" />
+        </div>
+    );
+}
+
+export default AboutGainz;
