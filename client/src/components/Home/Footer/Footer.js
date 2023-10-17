5cd16bc7c8fde22741389c44e969d11cccf25a76
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-17 11:52:50 +0530

    Create Footer.js

---
+++ client/src/components/Home/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <div className="CopyrightText">© GAINZ - 2023</div>
+            <div className="SocialLinks">
+                <div className="SocialLink">Github</div>
+                <div className="SocialLink">Linkedin</div>
+                <div className="SocialLink">Instagram</div>
+                <div className="SocialLink">Email</div>
+                <div className="SocialLink">Privacy Policy</div>
+                <div className="SocialLink">Terms of Use</div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
