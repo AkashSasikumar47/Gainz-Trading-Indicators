e6de01bf65cdc9bd75a2d0f3e47ff4ee6c14f420
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-17 11:52:20 +0530

    Create Hero.js

---
+++ client/src/components/Home/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="Hero">
+            <div className="HeroTextContainer">
+                <div className="HeroTitle">Welcome to GAINZ</div>
+                <div className="HeroSubtitle">Your Ultimate Trading Indicator Resource</div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
