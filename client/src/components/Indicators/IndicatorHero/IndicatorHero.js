89ca4e073910a3c17b7bb692db19a699f3499429
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-21 21:02:29 +0530

    Create IndicatorHero.js

---
+++ client/src/components/Indicators/IndicatorHero/IndicatorHero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './IndicatorHero.css';
+
+const IndicatorHero = () => {
+    return (
+        <div className="indicator-hero-container">
+            <div className="indicator-hero-content">
+                <h1 className="indicator-hero-title">Indicators</h1>
+                <p className="indicator-hero-description">
+                    Explore our trading indicators to make informed decisions in the stock market. Click on an indicator to learn more.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default IndicatorHero;
