19b20d152af4ee86dc9c6344ea2d80eb4381ab32
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-21 12:43:08 +0530

    Create ContactUs.js

---
+++ client/src/components/Contact/ContactUs/ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ContactUs.css';
+
+function ContactUs() {
+    return (
+        <div className="ContactUs">
+            <div className="Content">
+                <div className="Title">Contact Us</div>
+                <div className="Description">
+                    Feel free to get in touch with us if you have any questions, comments, or just want to say hello. We'd love to hear from you.
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ContactUs;
