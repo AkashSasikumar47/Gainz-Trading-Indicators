02c08c8a8518b17ea779fbb30f58e66424d289d5
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-21 21:07:18 +0530

    Update Footer

--- client/src/components/Home/Footer/Footer.js
+++ client/src/components/Home/Footer/Footer.js
@@ -6,9 +6,8 @@ function Footer() {
         <div className="Footer">
             <div className="FooterSection ContactInfo">
                 <h3>Contact Us</h3>
-                <p>Email: <a href="mailto:contact@example.com">contact@example.com</a></p>
-                <p>Phone: <a href="tel:+11234567890">+1 (123) 456-7890</a></p>
-                <p>Address: 123 Main St, City, Country</p>
+                <p>Email: <a href="mailto:akashsasikumar47@gmail.com">akashsasikumar47@gmail.com</a></p>
+                <p>Phone: <a href="tel:+91 9946927980">+91 9946927980</a></p>
                 <p>&copy;GAINZ</p>
             </div>
             <div className="FooterSection QuickLinks">
@@ -16,23 +15,23 @@ function Footer() {
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About Us</a></li>
-                    <li><a href="/services">Services</a></li>
+                    <li><a href="/indicators">Indicators</a></li>
                     <li><a href="/contact">Contact Us</a></li>
                 </ul>
             </div>
             <div className="FooterSection LegalInfo">
                 <h3>Legal Information</h3>
                 <ul>
-                    <li><a href="/privacy-policy">Privacy Policy</a></li>
-                    <li><a href="/terms-of-use">Terms of Use</a></li>
+                    <li><a href="/privacy">Privacy Policy</a></li>
+                    <li><a href="/terms">Terms of Use</a></li>
                 </ul>
             </div>
             <div className="FooterSection QuickLinks">
                 <h3>Follow Us</h3>
                 <ul>
-                    <li><a href="https://github.com">Github</a></li>
-                    <li><a href="https://www.linkedin.com">Linkedin</a></li>
-                    <li><a href="https://www.instagram.com">Instagram</a></li>
+                    <li><a href="https://github.com/AkashSasikumar47">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/akash-sasikumar47">Linkedin</a></li>
+                    <li><a href="https://www.instagram.com/akash__sasikumar">Instagram</a></li>
                 </ul>
             </div>
         </div>
