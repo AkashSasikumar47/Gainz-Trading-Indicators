fdc20376c7d310ff2058e47b7fce42a7668eb339
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-21 12:43:32 +0530

    Update PrivacyPolicy.js

--- client/src/components/Privacy/PrivacyPolicy/PrivacyPolicy.js
+++ client/src/components/Privacy/PrivacyPolicy/PrivacyPolicy.js
@@ -4,59 +4,47 @@ import './PrivacyPolicy.css';
 const PrivacyPolicy = () => {
     return (
         <div className="privacy-policy-container">
-            <div className="title-section">
-                <div className="title">Privacy Policy</div>
-                <div className="effective-date">Effective Date: 01/10/2023</div>
+            <div className="section">
+                <h1>Privacy Policy</h1>
+                <p>Effective Date: 01/10/2023</p>
             </div>
             <div className="section">
-                <div className="section-title">Overview</div>
-                <div className="section-content">
-                    Welcome to GAINZ. We're dedicated to respecting your privacy while you explore our platform. This simplified Privacy Policy outlines how we handle your information.
-                </div>
+                <h2>Overview</h2>
+                <p>Welcome to GAINZ. We're dedicated to respecting your privacy while you explore our platform. This simplified Privacy Policy outlines how we handle your information.</p>
             </div>
             <div className="section">
-                <div className="section-title">Information We Collect</div>
-                <div className="section-content">
-                    Contact Information: We may collect your name and email address if you contact us.<br />
-                    Usage Information: We collect data on how you use GAINZ to improve our content and user experience.
-                </div>
+                <h2>Information We Collect</h2>
+                <ul>
+                    <li>Contact Information: We may collect your name and email address if you contact us.</li>
+                    <li>Usage Information: We collect data on how you use GAINZ to improve our content and user experience.</li>
+                </ul>
             </div>
             <div className="section">
-                <div className="section-title">How We Use Your Information</div>
-                <div className="section-content">
-                    Communication: Your contact information is used to respond to your inquiries and messages.<br />
-                    Platform Improvement: Usage data helps us enhance GAINZ for a better user experience.
-                </div>
+                <h2>How We Use Your Information</h2>
+                <ul>
+                    <li>Communication: Your contact information is used to respond to your inquiries and messages.</li>
+                    <li>Platform Improvement: Usage data helps us enhance GAINZ for a better user experience.</li>
+                </ul>
             </div>
             <div className="section">
-                <div className="section-title">Data Sharing</div>
-                <div className="section-content">
-                    We don't share your personal information with external parties.
-                </div>
+                <h2>Data Sharing</h2>
+                <p>We don't share your personal information with external parties.</p>
             </div>
             <div className="section">
-                <div className="section-title">Security</div>
-                <div className="section-content">
-                    We take basic security measures to protect your information, but please note that online data transmission is not entirely secure.
-                </div>
+                <h2>Security</h2>
+                <p>We take basic security measures to protect your information, but please note that online data transmission is not entirely secure.</p>
             </div>
             <div className="section">
-                <div className="section-title">Your Choices</div>
-                <div className="section-content">
-                    You can reach out to us if you wish to review or update your information.
-                </div>
+                <h2>Your Choices</h2>
+                <p>You can reach out to us if you wish to review or update your information.</p>
             </div>
             <div className="section">
-                <div className="section-title">Changes to This Policy</div>
-                <div className="section-content">
-                    We may update this Privacy Policy, and changes will be posted on this page.
-                </div>
+                <h2>Changes to This Policy</h2>
+                <p>We may update this Privacy Policy, and changes will be posted on this page.</p>
             </div>
             <div className="section">
-                <div className="section-title">Contact Us</div>
-                <div className="section-content">
-                    If you have any privacy concerns or questions, please don't hesitate to <span className="contact-link">contact us</span>.
-                </div>
+                <h2>Contact Us</h2>
+                <p>If you have any privacy concerns or questions, please don't hesitate to <a href="/contact">contact us</a>.</p>
             </div>
         </div>
     );
