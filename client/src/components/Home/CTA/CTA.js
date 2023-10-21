967c076dbbfa9138dc739dd512be02f52c22a4ea
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-21 21:07:01 +0530

    Update CTA

--- client/src/components/Home/CTA/CTA.js
+++ client/src/components/Home/CTA/CTA.js
@@ -4,11 +4,12 @@ import './CTA.css';
 function CTA() {
     return (
         <div className="CTA">
-            <h2>Ready to Get Started?</h2>
+            <h2>Ready to Dive In?</h2>
+            <h3>Let's Make Those Gains Together</h3>
             <p>
-                Explore our platform and start your journey to financial success.
+                Start exploring our indicator repositories and gain a competitive edge in the stock market. Whether you're new to trading or an experienced investor, GAINZ has something valuable to offer.
             </p>
-            <button className="CTAButton">Get Started</button>
+            <button className="button">Learn More</button>
         </div>
     );
 }
