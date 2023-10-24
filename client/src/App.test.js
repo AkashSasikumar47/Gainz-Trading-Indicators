ed8105fb336c1ea915605ef1beb79ca6fa1ba403
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-24 20:04:20 +0530

    Update code

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Gainz Trading Indicators link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Gainz Trading Indicators/i);
   expect(linkElement).toBeInTheDocument();
 });
