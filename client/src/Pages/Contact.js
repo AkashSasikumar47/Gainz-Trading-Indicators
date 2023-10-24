56afd7810c541f5bcb25f66393b5ba2dad6482be
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-24 20:03:40 +0530

    Update Contact.js

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -17,4 +17,7 @@ function Contact() {
     );
 }
 
+Contact.title = "Contact Gainz Trading Indicators - Get in Touch with Us";
+Contact.description = "Contact Gainz Trading Indicators to get in touch with our team. We're here to answer your questions and provide assistance related to our trading indicators.";
+
 export default Contact;
