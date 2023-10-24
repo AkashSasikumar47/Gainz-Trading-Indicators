4d8b39c523bdd8d45b287884d4927b2d8e3fabb4
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-10-24 20:03:58 +0530

    Update Explore.js

--- client/src/components/Home/Explore/Explore.js
+++ client/src/components/Home/Explore/Explore.js
@@ -15,61 +15,17 @@ function Explore() {
                 <div className="ExploreCard">
                     <div className="ExploreCardImage">
                         <img
-                            src="/Assets/sample_images/img-1.jpg"
+                            src="/Assets/sample_images/Explore_section.png"
                             alt="A scenic mountain landscape"
                         />
                     </div>
                     <div className="ExploreCardTitle">Perfect for Beginners</div>
                     <div className="ExploreCardDescription">
-                        For those just starting, this repository offers beginner-friendly technical indicators coded in Pine Script and compatible with TradingView. Dive into the world of trading with confidence.
+                        Explore complex market insights with our expertly crafted indicators.
                     </div>
                 </div>
             </div>
 
-            <div className="ExploreSection">
-                <div className="ExploreCard">
-                    <div className="ExploreCardImage">
-                        <img
-                            src="/Assets/sample_images/img-2.jpg"
-                            alt="A scenic mountain landscape"
-                        />
-                    </div>
-                    <div className="ExploreCardTitle">Elevate Your Game</div>
-                    <div className="ExploreCardDescription">
-                        For traders looking to take it up a notch, these intermediate-level indicators provide insights into market trends, volatility, and potential price movements. Fine-tune your strategies and make more informed decisions.
-                    </div>
-                </div>
-            </div>
-
-            <div className="ExploreSection">
-                <div className="ExploreCard">
-                    <div className="ExploreCardImage">
-                        <img
-                            src="/Assets/sample_images/img-3.jpg"
-                            alt="A scenic mountain landscape"
-                        />
-                    </div>
-                    <div className="ExploreCardTitle">For the Experts</div>
-                    <div className="ExploreCardDescription">
-                        Designed for the experienced, this repository offers a comprehensive collection of advanced technical indicators. Analyze price movements, identify trends, and predict potential market reversals.
-                    </div>
-                </div>
-            </div>
-
-            <div className="ExploreSection">
-                <div className="ExploreCard">
-                    <div className="ExploreCardImage">
-                        <img
-                            src="/Assets/sample_images/img-1.jpg"
-                            alt="A scenic mountain landscape"
-                        />
-                    </div>
-                    <div className="ExploreCardTitle">Customize Your Strategy</div>
-                    <div className="ExploreCardDescription">
-                        Tailor your trading strategies with these custom indicators. Unlock your trading potential, make informed decisions, and succeed in the dynamic world of financial markets.
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
