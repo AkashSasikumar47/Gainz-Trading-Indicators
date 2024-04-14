5cfefca3826fe9484302ef1c867289f92120fffa
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-04-14 23:15:01 +0530

    Added components

--- gainz-trading-indicators/app/page.tsx
+++ gainz-trading-indicators/app/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import React from 'react';
+import Head from 'next/head';
+
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Footer from './components/Footer/Footer';
+
+import '../app/globals.css';
+
+const HomePage: React.FC = () => {
   return (
-    <h1 className="text-3xl text-blue-500 font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <Head>
+        <title>GAINZ | Your Ultimate Trading Indicator Resource</title>
+        <meta name="description" content="Gainz Trading Indicators is your ultimate resource for a collection of technical indicators to enhance your trading strategies. Whether you're a beginner or an experienced trader, Gainz offers a range of indicators to help you make informed decisions." />
+        <meta name="keywords" content="trading indicators, technical analysis, stock market, trading strategies, beginner trader, experienced trader" />
+        <link rel="icon" href="/favicon.ico?v=2" />
+      </Head>
 
-export default page
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
